db81c57d069e7acf42398548c3f871f65cfcc8c1
诚远 <370281698@qq.com>
2022-07-11 15:00:25 +0800

    git文档编写完成

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,51 +1,48 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 export default defineConfig({
-    lang: 'zh-CN',
-    title: 'Sun Note',
-    description: 'Vite & Vue powered static site generator.',
-    head: [['link', {rel: 'icon', href: '/logo.svg'}]],
-    appearance: true,
-    lastUpdated: true,
-    markdown: {
-        theme: 'material-palenight',
-        lineNumbers: true
+  lang: "zh-CN",
+  title: "Sun Note",
+  description: "Vite & Vue powered static site generator.",
+  head: [["link", { rel: "icon", href: "/logo.svg" }]],
+  appearance: true,
+  lastUpdated: true,
+  markdown: {
+    theme: "material-palenight",
+    lineNumbers: true,
+  },
+  themeConfig: {
+    // Nav
+    logo: { light: "/logo.svg", dark: "/logo-dark.svg" },
+    siteTitle: "SunNote",
+    nav: [
+      { text: "Java Web", link: "/web/java-web", activeMatch: "/web" },
+      { text: "Tools", link: "/tools/git" },
+    ],
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+    // SideBar
+    sidebar: {
+      "/tools/": [
+        {
+          text: "Develop Tools",
+          items: [
+            // This shows `/guide/index.md` page.
+            { text: "Git", link: "/tools/git" }, // /guide/index.md
+          ],
+        },
+      ],
     },
-    themeConfig: {
-        // Nav
-        logo: { light: '/logo.svg', dark: '/logo-dark.svg' },
-        siteTitle: 'SunNote',
-        nav: [
-            { text: 'Java Web', link: '/web/java-web', activeMatch: '/web' },
-            { text: 'Tools', link: '/tools/git'},
-        ],
-        socialLinks: [
-            { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
-            { icon: 'twitter', link: '...' },
-            { icon: 'discord', link: '...' }
-        ],
-
-        // SideBar
-        sidebar: {
-            '/tools/': [
-                {
-                    text: 'Develop Tools',
-                    items: [
-                        // This shows `/guide/index.md` page.
-                        { text: 'Git', link: '/tools/git' }, // /guide/index.md
-                    ]
-                }
-            ],
-        },
-        // Edit Link
-        editLink: {
-            pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
-            text: 'Edit this page on GitHub'
-        },
-        footer: {
-            message: 'Released under the MIT License.',
-            copyright: 'Copyright © 2022-present Chengyuan Sun'
-        },
-        lastUpdatedText: 'Updated Date'
-    }
-})
+    // Edit Link
+    editLink: {
+      pattern: "https://github.com/vuejs/vitepress/edit/main/docs/:path",
+      text: "在GitHub上编辑此页面",
+    },
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2022-present Chengyuan Sun",
+    },
+    lastUpdatedText: "更新日期",
+  },
+});
